7e25f7a4f563657c6f25b3e1d7b503d6cd15f49d
Eba <enkhba.dendev@rakuten.com>
2022-02-08 22:26:31 +0900

    UI navbar design is improved

--- ui/src/app/App.tsx
+++ ui/src/app/App.tsx
@@ -15,19 +15,38 @@ function App() {
   return (
     <Router>
       <Layout>
-        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Header
+          style={{
+            position: 'fixed',
+            zIndex: 1,
+            width: '100%',
+            backgroundColor: '#e76f51',
+          }}
+        >
+          <Menu
+            style={{ backgroundColor: '#e76f51' }}
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+          >
             <Link to="/">
-              <Menu.Item key="0">My Logo</Menu.Item>
+              <Menu.Item key="0" style={{ color: 'white', fontSize: '20px' }}>
+                My Logo
+              </Menu.Item>
             </Link>
             <Link to="/dic">
-              <Menu.Item key="1">Dictionary</Menu.Item>
+              <Menu.Item key="1" style={{ color: 'white', fontSize: '20px' }}>
+                Dictionary
+              </Menu.Item>
             </Link>
             <Link to="/typing">
-              <Menu.Item key="2">Typing</Menu.Item>
+              <Menu.Item key="2" style={{ color: 'white', fontSize: '20px' }}>
+                Typing
+              </Menu.Item>
             </Link>
             <Link to="/games">
-              <Menu.Item key="3">Games</Menu.Item>
+              <Menu.Item key="3" style={{ color: 'white', fontSize: '20px' }}>
+                Games
+              </Menu.Item>
             </Link>
           </Menu>
         </Header>
