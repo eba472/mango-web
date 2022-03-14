c65f49626eac5af30700b71336d543e7f4487f04
Eba <enkhba.dendev@rakuten.com>
2022-03-14 19:31:17 +0900

    change

--- src/app/components/header/registration/dataFire.js
+++ src/app/components/header/registration/dataFire.js
@@ -1,8 +1,36 @@
-var fs = require('fs');
 
-console.log(1)
+var fs = require('fs')
 
-var data = fs.readFileSync('/Users/a817011/Desktop/Development/Mango/dict_app/src/app/components/header/registration/dataFire.js', 'utf8');
-console.log("1", data.toString());    
+firebase.initializeApp({
+  apiKey: 'AIzaSyDOpnWwgDajQaMpmncBUL0Rv5CcLMUsVkk',
+  authDomain: 'aplus-4ae14.firebaseapp.com',
+  projectId: 'aplus-4ae14',
+  storageBucket: 'aplus-4ae14.appspot.com',
+  messagingSenderId: '1065455070773',
+  appId: '1:1065455070773:web:5f16f442e1ed6b28becb7c',
+  measurementId: 'G-YZLRQ0WT1X',
+})
 
-console.log(2)
+function myFunction() {
+    var data = fs.readFileSync(
+      '/Users/enkhba.dendev/Desktop/typingPracticeApp/src/app/components/header/registration/database.txt',
+      'utf8'
+    )
+  
+    const listOfwords = data.split('\n').filter((word: string) => word !== '')
+  
+    for (let i = 0; i < 4; i += 2) {
+      console.log(listOfwords[i], listOfwords[i + 1])
+      firestore
+        .collection('engMonDict')
+        .doc(`${listOfwords[i]}`)
+        .set({
+          mn: `${listOfwords[i + 1]}`,
+        })
+        .then(() => {
+          console.log('haha')
+        })
+    }
+  }
+
+myFunction()
