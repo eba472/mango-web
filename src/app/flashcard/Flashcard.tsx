9c202d8d190a0278791aaa6d1219fc6ea7bc7804
Xocoo <bhosoo0523@gmail.com>
2022-03-21 01:13:32 +0800

    Flashcard section functionality

--- src/app/flashcard/Flashcard.tsx
+++ src/app/flashcard/Flashcard.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useState }from 'react'
+import './static/css/style.css'
 import FlashcardComponent from './components/FlashcardComponent'
+import Button from './components/Button'
+import { useTranslation } from 'react-i18next'
+
 const savedWordsByUser = [
   {
     word: 'apple',
@@ -18,6 +22,27 @@ const savedWordsByUser = [
   },
 ]
 const Flashcard = () => {
+  const { t } = useTranslation('common')
+  const flashcardsData = savedWordsByUser
+  const [showFront, setShowFront] = useState(true)
+  var dataLength = flashcardsData.length
+  let component
+  var [ index, setIndex ] = useState(0)
+  if ( index < flashcardsData.length ) {
+    component = (
+      <div>
+        <FlashcardComponent flashcards={flashcardsData} showFront={showFront} setShowFront={setShowFront} index={index} />
+        <Button index={index} setIndex={setIndex} dataLength={dataLength} />
+      </div>
+      )
+  } else {
+    component = (
+      <div>
+        <p>{t('Flashcard.noMoreFlashcards')}!</p>
+        <Button index={index} setIndex={setIndex} dataLength={dataLength} />
+      </div>
+    )
+  }
   return (
     <div
       style={{
@@ -25,9 +50,9 @@ const Flashcard = () => {
         justifyContent: 'center',
         marginTop: '40px',
         fontSize: '50px !important',
-      }}>
-      This is a flashcard app.
-      <FlashcardComponent />
+      }}
+      >
+      { component }
     </div>
   )
 }
